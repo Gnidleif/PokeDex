cd62252521a4b11b9da09e8f0bb389213cc00d0e
Gnidleif <gnidleif@gmail.com>
2020-06-24 16:22:27 +0200

    Update main.py

--- Pokedex/main.py
+++ Pokedex/main.py
@@ -13,19 +13,20 @@ def readFromCsv():
 
 def compileStats(pokemon):
     return {
-        'No': pokemon['Number'],
+        'Number': pokemon['Number'],
         'Name': pokemon['Name'],
         'Type': "/".join(filter(None, [pokemon['Type1'], pokemon['Type2']])),
-        'Quarter': ', '.join(filter(lambda x: pokemon[x] == '0.25' and x != 'Number', [x for x in pokemon])),
-        'Half': ', '.join(filter(lambda x: pokemon[x] == '0.5' and x != 'Number', [x for x in pokemon])),
-        'Double': ', '.join(filter(lambda x: pokemon[x] == '2' and x != 'Number', [x for x in pokemon])),
-        'Quadruple': ', '.join(filter(lambda x: pokemon[x] == '4' and x != 'Number', [x for x in pokemon])),
+        'Immune': ','.join(filter(lambda x: pokemon[x] == '0.0' and x != 'Number', [x for x in pokemon])),
+        'Quarter': ','.join(filter(lambda x: pokemon[x] == '0.25' and x != 'Number', [x for x in pokemon])),
+        'Half': ','.join(filter(lambda x: pokemon[x] == '0.5' and x != 'Number', [x for x in pokemon])),
+        'Double': ','.join(filter(lambda x: pokemon[x] == '2.0' and x != 'Number', [x for x in pokemon])),
+        'Quadruple': ','.join(filter(lambda x: pokemon[x] == '4.0' and x != 'Number', [x for x in pokemon])),
     }
 
 def main(args):
     if len(args) == 0:
         return
-    search = args[0]
+    search = " ".join(args)
     rgx = re.compile(r".*{}.*".format(search), re.IGNORECASE)
     pokemons = readFromCsv()
     for key in pokemons:
