9c05a92326c7b368caa47950192aa3a4acdcf915
Tim Fielding <gnidleif@gmail.com>
2020-06-25 10:07:11 +0200

    restructure

--- Pokedex/__main__.py
+++ Pokedex/__main__.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import os, csv, re
+from pokedex import update
 
-def readFromCsv():
+def readPokedex():
     pokemons = {}
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(__location__, 'poketypes.csv'), 'r', newline='', encoding='utf-8') as f:
+    with open(os.path.join(__location__, 'pokedex.csv'), 'r', newline='', encoding='utf-8') as f:
         reader = csv.DictReader(f)
         for row in reader:
             key = "{} {}".format(row['Number'], row['Name'])
@@ -16,19 +17,20 @@ def compileStats(pokemon):
         'Number': pokemon['Number'],
         'Name': pokemon['Name'],
         'Type': "/".join(filter(None, [pokemon['Type1'], pokemon['Type2']])),
-        'Immune': ','.join(filter(lambda x: pokemon[x] == '0.0' and x != 'Number', [x for x in pokemon])),
-        'Quarter': ','.join(filter(lambda x: pokemon[x] == '0.25' and x != 'Number', [x for x in pokemon])),
-        'Half': ','.join(filter(lambda x: pokemon[x] == '0.5' and x != 'Number', [x for x in pokemon])),
-        'Double': ','.join(filter(lambda x: pokemon[x] == '2.0' and x != 'Number', [x for x in pokemon])),
-        'Quadruple': ','.join(filter(lambda x: pokemon[x] == '4.0' and x != 'Number', [x for x in pokemon])),
+        'Immune': ','.join(filter(lambda x: pokemon[x] == '0.0', [x for x in pokemon])),
+        'Quarter': ','.join(filter(lambda x: pokemon[x] == '0.25', [x for x in pokemon])),
+        'Half': ','.join(filter(lambda x: pokemon[x] == '0.5', [x for x in pokemon])),
+        'Double': ','.join(filter(lambda x: pokemon[x] == '2.0', [x for x in pokemon])),
+        'Quadruple': ','.join(filter(lambda x: pokemon[x] == '4.0', [x for x in pokemon])),
     }
 
 def main(args):
     if len(args) == 0:
+        update()
         return
     search = " ".join(args)
+    pokemons = readPokedex()
     rgx = re.compile(r".*{}.*".format(search), re.IGNORECASE)
-    pokemons = readFromCsv()
     for key in pokemons:
         if rgx.match(key):
             print(compileStats(pokemons[key]))
--- Pokedex/pokedex.py
+++ Pokedex/pokedex.py
@@ -5,45 +5,95 @@ import re, csv, os
 
 class Pokemon:
     def __init__(self, num, name, types):
-        self._num = num
-        self._name = name
-        self._types = types
+        self.Number = num
+        self.Name = name
+        self.Types = types
 
-    def __repr__(self):
-        return "#{}: {} - {}".format(self._num, self._name, self._types)
-
-def fetchPokemonTable(url):
+def fetchTable(url):
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    web = urlopen(req).read()
-    webstr = web.decode('utf-8')
+    web = urlopen(req).read().decode('UTF-8')
     p = HTMLTableParser()
-    p.feed(webstr)
+    p.feed(web)
     return p.tables[0][1:]
 
-def createPokemons(rows):
+def createPokemons(table):
+    rgx = re.compile(r"^([\w\s\.']+)\s{2}(.+)$")
     pokemons = []
-    for row in rows:
+    for row in table:
         num = row[0].rjust(3, '0')
         name = row[1]
         types = row[2].split()
-        rgx = re.compile(r"^(\w+)\s{2}(.+)$")
-        m = rgx.match(row[1])
-        if m:
-            name = "{} ({})".format(m.group(1), m.group(2))
+        match = rgx.match(row[1])
+        if match:
+            if match.group(1) in match.group(2):
+                name = match.group(2)
+            else:
+                name = "{} {}".format(match.group(2), match.group(1))
+
         pokemons.append(Pokemon(num, name, types))
     return pokemons
 
-def writeToCsv(pokemons):
+def createTypes(table):
+    types = {}
+    keys = list(zip(*table))[0]
+    values = list(zip(*table))[1:]
+    keyVals = dict(zip(keys, values))
+    for k in keyVals:
+        test[k] = dict(zip(keys, keyVals[k]))
+        switch = {
+            '': 1.0,
+            '0': 0.0,
+            '½': 0.5,
+            '2': 2.0,
+        }
+        for l in keyVals[k]:
+            keyVals[k][l] = switch[keyVals[k][l]]
+        types[k] = keyVals[k]
+    return types
+
+def getTypeRelationships(pokemon, types):
+    result = {}
+    for t in pokemon.Types:
+        row = types[t]
+        for k in row:
+            if k not in result:
+                result[k] = row[k]
+            else:
+                result[k] *= row[k]
+    return result
+
+def writePokedex(pokemons, types, relationships):
     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    with open(os.path.join(__location__, 'pokemon.csv'), 'w', newline='', encoding='utf-8') as f:
-        fieldnames = ['number', 'name', 'type1', 'type2']
+    with open(os.path.join(__location__, 'pokedex.csv'), 'w', newline='', encoding='utf-8') as f:
+        fieldnames = [
+            'Number', 
+            'Name', 
+            'Type1', 
+            'Type2', 
+        ]
+        fieldnames.extend(types)
         writer = csv.DictWriter(f, fieldnames = fieldnames)
         writer.writeheader()
-        for row in pokemons:
-            writer.writerow({'number': row._num, 'name': row._name, 'type1': row._types[0], 'type2': row._types[1] if len(row._types) > 1 else ''})
+        for i in range(len(pokemons)):
+            toWrite = {
+                'Number': pokemons[i].Number,
+                'Name': pokemons[i].Name,
+                'Type1': pokemons[i].Types[0],
+                'Type2': pokemons[i].Types[1] if len(pokemons[i].Types) > 1 else '',
+            }
+            toWrite.update(relationships[i])
+            writer.writerow(toWrite)
 
-def updatePokemons():
+def update():
     url = 'https://pokemondb.net/pokedex/all'
-    web = fetchPokemonTable(url)
-    pokemons = createPokemons(web)
-    writeToCsv(pokemons)
+    table = fetchTable(url)
+    pokemons = createPokemons(table)
+
+    url = 'https://pokemondb.net/type'
+    table = fetchTable(url)
+    types = createTypes(table)
+
+    relationships = []
+    for i in range(len(pokemons)):
+        relationships.append(getTypeRelationships(pokemons[i], poketypes))
+    writePokedex(pokemons, poketypes.keys(), relationships)
